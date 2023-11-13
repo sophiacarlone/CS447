8f3d6be4b2eb6fe09d767fcd0f2fef4d3497ebd9
sophiacarlone <sophiamcar@gmail.com>
2023-11-13 16:32:32 -0500

    it vanished before

--- hw1/graph_node.cpp
+++ hw1/graph_node.cpp
@@ -1,5 +1,3 @@
-//Sophia Carlone
-
 #include<iostream>
 #include<vector>
 
@@ -9,13 +7,28 @@ class graph_node{
 	private:
 		switch_num_;
 		vector<graph_node*> implies_; //pointers to implicaitons
+		bool seen_;
 	public:
-		void track();	
+		void BFS(const graph_node* origin, int size);	
 };
 
 
-void graph_node::track(){
-	for(int i  = 0; i < implies_.size(); i++){
-		
-	} 
+void graph_node::BFS(const graph_node* origin, int size){
+	graph_node[size] fringe;
+	int start = 0;
+	int next_space = 1;
+	fringe[start] = origin;
+	do{
+		for(int i  = 0; i < implies_.size(); i++){
+			fringe[next_space] = implies_[i];
+			next_space++;
+		}
+		fringe[start].seen_ = true;
+		start++;
+	}while(fringe[start] == false);
+
+	for(int i = 0; i < size; i++){
+		cout << fringe[i] << " ";
+	}
+	cout << endl;
 }
