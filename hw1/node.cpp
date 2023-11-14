ba1298b158c68fd49e497c66d685380670aee1a8
sophiacarlone <sophiamcar@gmail.com>
2023-11-14 14:53:37 -0500

    move to another machine

--- hw1/node.cpp
+++ hw1/node.cpp
@@ -13,7 +13,7 @@ class node{ //class specifically for tracking and representation of the lightbul
 		vector<node *> edges_; //pointers to what this node should imply. This of this as edges QUESTION: should this be in public?
 		node(int id); //constructor
 		void Add_edges(const vector<node *>& implies); //add edges of the vector
-		//T0DO: add edge one at a time
+		//TOD0: add edge one at a time
 		void print_edges(); 
 		int getSwitchID(){return switch_ID_;};
 		int getNumEdges(){return edges_.size();}; //TOD0: get rid of this
@@ -40,7 +40,8 @@ void node::print_edges(){
 } 
 
 //prototypes
-void BFS(node * origin, int size);
+//void BFS(node * origin, int size);
+bool Track(node * origin, int size);
 
 int main(){
 	node n1 (1);
@@ -50,6 +51,7 @@ int main(){
 	node n5 (5);
 	node n6 (6);
 	node n7 (7);
+	node nneg1 (-1);
 
 	vector<node *> fringe;
 	fringe.push_back(&n2);	
@@ -64,21 +66,28 @@ int main(){
 	
 	n2.Add_edges(f2);
 
-	n1.print_edges();
-	n2.print_edges();
+	//n1.print_edges();
+	//n2.print_edges();
 
 	vector<node *>f3;
 	f3.push_back(&n1);
 	f3.push_back(&n7);
+	f3.push_back(&nneg1);
 	n4.Add_edges(f3);
 	
-	n4.print_edges();
+	//n4.print_edges();
 
-	BFS(&n1, 7);
+	//BFS(&n1, 7);
+	cout << "tracking now" << endl;
+	bool found = Track(&n1, 8);
+	cout << "tracking done" << endl;
+
+	cout << "was it found? " << found << endl;	
 
 	return 0;
 }
 
+/*
 void BFS(node * origin, int size){
 	node * fringe[size];
 	int curr_head = 0;
@@ -110,9 +119,49 @@ void BFS(node * origin, int size){
 		cout << fringe[i]->getSwitchID() << " ";
 	}
 	cout << endl;		
+}*/
+
+bool Track(node * origin, int size){ //track and chase
+	//cout << "hit a" << endl;
+	//BFS
+	node * fringe[size];
+	int curr_head = 0;
+	int next_space = 1;
+	fringe[curr_head] = origin;
+	
+	do{
+		//if(fringe[curr_head]->getSeen()){
+		//	continue;
+		//} 
+		//cout << "hit B" << endl;
+		for(int i = 0; i < fringe[curr_head]->getNumEdges(); i++){
+			if(!(fringe[curr_head]->edges_[i]->getSeen())){
+			fringe[next_space] = fringe[curr_head]->edges_[i];
+			//cout << fringe[next_space]->getSwitchID() << " ";
+			next_space++;
+			}
+			//cout << endl << "next space # is " << next_space << endl;
+			for(int j = 0; j < size; j++){
+				cout << fringe[j]->getSwitchID() << " ";
+			}
+			cout << endl;	
+		}
+		fringe[curr_head]->setSeen(true);
+		curr_head++;
+	}while(curr_head < size);
+/*
+	for(int i = 0; i < size; i++){
+		cout << fringe[i]->getSwitchID() << " ";
+	}
+	cout << endl;
+*/
+	//END OF BFS
+	
+	for(int i = 0; i < size; i++){
+		if(fringe[i]->getSwitchID() == (-1 * origin->getSwitchID())) return true;
+	}
+	return false;	
 }
-
-
 
 
 
