dc434a00e8c6bf7de72936b81c8e4f66d79e9671
sophiacarlone <sophiamcar@gmail.com>
2023-11-25 22:34:30 -0500

    some clean up. Seg fault in Track()

--- hw1/thatothermain.cpp
+++ hw1/thatothermain.cpp
@@ -13,14 +13,12 @@ class node{ //class specifically for tracking and representation of the lightbul
 		bool seen_; //has it been accounted for in BFS QUESTION: will this be necessary?
 		int value_; //hold the ideal value based on tracking results (0/1)
 		bool confirmed_; //Is the value held by this node confirmed?
-		//vector<node *> edges_; //pointers to what this node should imply. This of this as edges QUESTION: should this be in public?
 	public:
 		vector<node *> edges_; //pointers to what this node should imply. This of this as edges QUESTION: should this be in public?
 		node();
 		//node(int id); //constructor
 		bool getConfirmed(){return confirmed_;};
 		void Add_edges(const vector<node *>& implies); //add edges of the vector
-		//TOD0: add edge one at a time
 		void print_edges(); 
 		int getSwitchID(){return switch_ID_;};
 		void setSwitchID(int x){switch_ID_ = x;};
@@ -54,84 +52,10 @@ void node::print_edges(){
 map<int, node> graph; //graph that will hold all implications
 
 //PROTOTYPES
+vector <pair <int,int> > lightbulbs();
 bool Track(node * origin, int size);
 void Result(node * origin, bool x2negx, bool negx2x);
 
-vector <pair <int,int> > lightbulbs(){
-    int bulbs; //total bulbs
-    int switches; //total switches
-    int curr_switch1; //switch being looked at at one time
-    int curr_switch2; //switch being looked at at one time
-    vector<pair <int,int> > bulb_confs; // bulb configuration for checking later
-
-    ifstream in;
-    in.open("instance.txt");
-    in >> switches >> bulbs;
-    pair <int,int> cheese (switches,bulbs);
-    bulb_confs.push_back(cheese);
-    node switchNum; //it goes back up here to call the implicit deallocator
-
-    for(int i = 1; i <= switches; i++){ 
-	switchNum.setSwitchID(i);
-	graph[i] = switchNum;
-	switchNum.setSwitchID(-1*i);
-	graph[-1*i] = switchNum;
-
-    }
-
-//for testing
-/*    for(int i = 1; i <= switches; i++){ //QUESTION: why -1?
-	//switchNum.setValue(i);
-	cout << graph[i].getSwitchID() << " ";
-    }
-	cout << endl;
-*/
-    for (int i = 0; i < bulbs; i++){
-
-	//a | b
-        in >> curr_switch1; //get switch being looked at
-        in >> curr_switch2; //get switch being looked at
-        cheese.first = curr_switch1;
-        cheese.second = curr_switch2;
-        bulb_confs.push_back(cheese);
-/*
-	if(graph.find(curr_switch1) == graph.end()){ //see if its in
-		node nx = new node;
-		nx(curr_switch1); //TOD0: get rid of the set id since it will be the key of the map called graph
-		graph.insert(pair<int, node>(curr_switch, nx));
-	}
-	if(graph.find(curr_switch2) == graph.end()){ //see if its in
-		node nx = new node(curr_switch2); 
-		graph.insert(pair<int, node>(curr_switch2, nx));
-	}
-	if(graph.find(-1 * curr_switch1) == graph.end()){ //see if its in
-		node nx = new node(-1 * curr_switch1); 
-		graph.insert(pair<int, node>(-1 * curr_switch1, nx));
-	}
-	if(graph.find(-1 * curr_switch2) == graph.end()){ //see if its in
-		node nx = new node(-1 * curr_switch2); 
-		graph.insert(pair<int, node>(-1 * curr_switch2, nx));
-	}
-*/
-	//cout << curr_switch1 <<endl; // ~a->b
-	//cout << curr_switch2 << endl; // ~a->b
-	graph[-1*curr_switch1].edges_.push_back(&graph[curr_switch2]); // ~a->b
-	graph[-1*curr_switch2].edges_.push_back(&graph[curr_switch1]); // ~b->a
-
-    }
-/*
-    for(int i = 1; i <= switches; i++){ //QUESTION: why -1?
-	//switchNum.setValue(i);
-	cout << i << ": ";
-	graph[i].print_edges();
-	cout << -1*i << ": ";
-	graph[-1*i].print_edges();
-    }
-	cout << endl;
-  */ 
-    return bulb_confs;
-    //return graph;
-}
 
 //FUNCTIONS
 int main(){
@@ -140,19 +64,14 @@ int main(){
 	int bulbs = configuration.front().second;
 	int switches = configuration.front().first;
 	int tracked_posneg, tracked_negpos;
+	cout << switches << " " << bulbs << endl;
 
-/*    for(int i = 1; i <= switches; i++){ //QUESTION: why -1?
-	//switchNum.setValue(i);
-	graph[i].print_edges();
-	graph[-1*i].print_edges();
-    }
-	cout << endl;
-*/
     for(int i = 1; i <= switches; i++){ 
-//	cout << endl;
+	cout << "hit A" << endl;
 	tracked_posneg = Track(&(graph[i]), graph.size());
-//	cout << endl;
+	//cout << endl;
 	tracked_negpos = Track(&(graph[-1*i]), graph.size());
+	cout << "hit z" << endl;
 	Result(&(graph[i]), tracked_posneg, tracked_negpos);
     }
 	for (int i = 1; i<= switches; i++){
@@ -201,52 +120,104 @@ int main(){
 	return 0;
 }
 
+vector <pair <int,int> > lightbulbs(){
+    int bulbs; //total bulbs
+    int switches; //total switches
+    int curr_switch1; //switch being looked at at one time
+    int curr_switch2; //switch being looked at at one time
+    vector<pair <int,int> > bulb_confs; // bulb configuration for checking later
+
+    ifstream in;
+    in.open("instance.txt");
+    in >> switches >> bulbs;
+    pair <int,int> cheese (switches,bulbs);
+    bulb_confs.push_back(cheese);
+    node switchNum; //it goes back up here to call the implicit deallocator
+
+    for(int i = 1; i <= switches; i++){ 
+	switchNum.setSwitchID(i);
+	graph[i] = switchNum;
+	switchNum.setSwitchID(-1*i);
+	graph[-1*i] = switchNum;
+
+    }
+
+//for testing
+/*    for(int i = 1; i <= switches; i++){ //QUESTION: why -1?
+	//switchNum.setValue(i);
+	cout << graph[i].getSwitchID() << " ";
+    }
+	cout << endl;
+*/
+    for (int i = 0; i < bulbs; i++){
+
+	//a | b
+        in >> curr_switch1; //get switch being looked at
+        in >> curr_switch2; //get switch being looked at
+        cheese.first = curr_switch1;
+        cheese.second = curr_switch2;
+        bulb_confs.push_back(cheese);
+	graph[-1*curr_switch1].edges_.push_back(&graph[curr_switch2]); // ~a->b
+	graph[-1*curr_switch2].edges_.push_back(&graph[curr_switch1]); // ~b->a
+
+    }
+
+/*    for(int i = 1; i <= switches; i++){ //QUESTION: why -1?
+	//switchNum.setValue(i);
+	cout << i << ": ";
+	graph[i].print_edges();
+	cout << -1*i << ": ";
+	graph[-1*i].print_edges();
+    }
+	cout << endl;
+*/ 
+    return bulb_confs;
+    //return graph;
+}
+
 bool Track(node * origin, int size){ //track and chase
-//	cout << "switch id " << origin->getSwitchID() << " " << endl;
-//	cout << "edges: ";
-//	origin->print_edges();
+	cout << "hit 1" << endl;
 	if(origin->edges_.size() == 0) return false;
 	//BFS
+	origin->print_edges();
 	vector<node *> fringe;
-	//node * fringe[size];
 	int curr_head = 0;
-	//int next_space = 1;
+	bool ability_to_continue;
 	fringe.push_back(origin);
-	//fringe[curr_head] = origin;
-	
 	do{
-//		cout << "in do" << endl;
-//		cout << "curr head: " << curr_head << " fringe size: " << fringe.size() << endl;
 		//if something has no edges, we should break
-//		fringe[curr_head]->print_edges();
-//		cout << "HIIIIIII" << endl;
 		if(fringe[curr_head]->getNumEdges() == 0){ 
 			if(curr_head == fringe.size()-1) break;
 			continue;
+		//if a node has edges that have all been seen, we should break
+		ability_to_continue = false;
 		}
+		cout << fringe[curr_head]->getSwitchID() << "is said to have this amount of edges " << fringe[curr_head]->getNumEdges() << endl;
 		for(int i = 0; i < fringe[curr_head]->getNumEdges(); i++){
 			if(!(fringe[curr_head]->edges_[i]->getSeen())){
 				fringe.push_back(fringe[curr_head]->edges_[i]);
-				//fringe[next_space] = fringe[curr_head]->edges_[i];
-				//next_space++;
+				//cout << "what is being inserted: " << fringe[curr_head]->edges_[i]->getSwitchID() << endl;
+				fringe[curr_head]->edges_[i]->setSeen(true);
+				ability_to_continue = true;
 			}
 		}
+		//cout << fringe.size() << endl;
+		if(!ability_to_continue) break;
 		fringe[curr_head]->setSeen(true);
-//		cout << "hit 1" << endl;
 		curr_head++;
-//		for(int i = 0; i < fringe.size(); i++){
-//			cout << fringe[i]->getSwitchID() << " ";
-//		}
-//		cout << endl;
+/*		for(int i = 0; i < fringe.size(); i++){
+			cout << fringe[i]->getSwitchID() << " ";
+		}
+		cout << endl;*/
 //		fringe[curr_head]->print_edges();
 
-	}while(curr_head < size); //TOD0: fix
-/*
+	}while(curr_head < size); 
+
 	for(int i = 0; i < size; i++){
 		cout << fringe[i]->getSwitchID() << " ";
 	}
 	cout << endl;
-*/
+
 	//END OF BFS
 	
 	for(int i = 0; i < fringe.size(); i++){
@@ -257,6 +228,7 @@ bool Track(node * origin, int size){ //track and chase
 }
 
 void Result(node * origin, bool x2negx, bool negx2x){
+	cout << "HIT result" << endl;
 	if(x2negx && negx2x){
 		cout << "NO SOLUTION" << endl;
 		exit(0);
