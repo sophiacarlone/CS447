83f39bfee56ac75c386426755c25aa22f6b92a5e
Trevor <114937133+TrevoratorSavage@users.noreply.github.com>
2023-12-03 22:20:21 -0500

    cheese2.0

--- hw2/hw2.cpp
+++ hw2/hw2.cpp
@@ -1,123 +1,125 @@
 #include<iostream>
+#include <fstream>
 #include<vector>
 
 using namespace std;
 
 int main(){
 
-    double pxx = .75; // prob of staying on x if x was last thrown
-    double pxy = .25; // prob changing to y if x was last thrown
-    double pyy = .1;  // prob staying y if y was last thrown
-    double pyx = .9;  // prob changing y if y was last thrown
-    double xh = .6;   // probability of getting heads if throwing x
-    double xt = .4;   // probability getting tails when throwing x
-    double yh = .8;   // probability of getting heads when throwing y
-    double yt = .2;   // probability of getting tails when throwing y
-    double prob, probX, probY; // total probability, probability of coin x being the last coin flipped, probability of coin y being the last coin flipped
+    double pxx; // prob of staying on x if x was last thrown
+    double pxy; // prob changing to y if x was last thrown
+    double pyy;  // prob staying y if y was last thrown
+    double pyx;  // prob changing to x if y was last thrown
+    double xh;   // probability of getting heads if throwing x
+    double xt;   // probability getting tails when throwing x
+    double yh;   // probability of getting heads when throwing y
+    double yt;   // probability of getting tails when throwing y
+    string fname1,fname2, fname3, fname4;
+    double prob, probX, probY, actX, actY; // total probability, probability of coin x being the last coin flipped, probability of coin y being the last coin flipped
+    double sum;  // used in debugging,  prob x + prob y has to be 1, but it is not in all cases (hence the need for debugging)
+    vector <bool> result, observed; //result vector (0 is X 1 is Y) observed vector (0 is T, 1 is H)
+    bool x;
 
+    cout << "Filename 1:" << endl;
+    cin >> fname1;
+    ifstream in;
+    in.open(fname1);
+    in >> pxx >> pyx >> pxy >> pyy;
+    in.close();
+    in.clear();
 
-    double sum;  // used in debugging,  prob x + prob y has to be 1, but it is not in all cases (hence the need for debuggung)
+    cout << "Filename 2:" << endl;
+    cin >> fname2;
+    in.open(fname2);
+    in >> xh >> yh;
+    xt = 1 - xh;
+    yt = 1 - yh;
+    in.close();
+    in.clear();
 
-
+    cout << "Filename 3:" << endl;
+    cin >> fname3;
+    in.open(fname3);
    
-    double observed[20]; //observed vector (0 is T, 1 is H, array for testing, will be vector when getting from file)
-    
-    vector <double> result;//result vector (0 is X 1 is Y)
-    for (int i = 0; i< 20; i++){
-    observed[i]= (rand()%2); //randomly filling it
+    while (in >> x) {
+        observed.push_back(x);
     }
 
-
-    //first coin calculations
-    
+    //first coin calculations.   
 
     if (observed[0] == 0){
         prob = .5*(xt + yt); // if a tails is observed, the total possibility is .5(xt) + .5(yt)
         probX = (.5*xt)/prob; // of the total probabiltiy .5(probability of tails given x) is the chance that coin x was flipped
         probY = (.5*yt)/prob; // of the total probabiltiy .5(probability of tails given y) is the chance that coin y was flipped
 
-        sum = probX + probY;   //debug line
-        cout << probX << " first " << probY << " sum " << sum << endl; //debug line
-
         if (probX > probY)
             result.push_back(0); // if there is a higher probability of coin x being the one that was flipped, we put a 0 to represent that in the result vector
         else
             result.push_back(1); // if there is a higher probability of coin y being the one that was flipped, we put a 1 to represent that in the result vector
     }
 
-
     else 
         prob = .5*(xh + yh);  // if a heads is observed, the total possibility is .5(xh) + .5(yh)
-        probX = (.5*xh)/prob; // of the total probabiltiy .5(probability of heads given x) is the chance that coin x was flipped
-        probY = (.5*yh)/prob; // of the total probabiltiy .5(probability of heads given y) is the chance that coin y was flipped
-
-        sum = probX + probY; //debug line
-        cout << probX << " first " << probY << " sum " << sum << endl; //debug line
+        probX = (.5*xh)/prob; // of the total probability .5(probability of heads given x) is the chance that coin x was flipped
+        probY = (.5*yh)/prob; // of the total probability .5(probability of heads given y) is the chance that coin y was flipped
 
         if (probX > probY)
             result.push_back(0); // if there is a higher probability of coin x being the one that was flipped, we put a 0 to represent that in the result vector
         else
             result.push_back(1); // if there is a higher probability of coin y being the one that was flipped, we put a 1 to represent that in the result vector
 
-
-
     //rest of calculations
-    
 
-
-    for( int i = 1; i < 20; i++){ //make this while (input isn't empty) for real code
+    for( int i = 1; i < observed.size(); i++){ //make this while (input isn't empty) for real code
 
         if (observed[i] == 0){
 
-            prob = (probX*pxx*xt + probX*pxy*yt + probY*pyy*yt + probY*pyx*xt); //if a tails is observed, the total probability is:  (prob that last was x)(prob stays x)(prob tails w/ x) +
- // (prob that last was x)(prob changes to y)(prob tails w/ y) + (prob that last was y)(prob stays y)(prob tails w/ y) + (prob that last was y)(prob changes to x)(prob tails w/ x)
-
-            probX = ((probX*pxx*xt)+(probY*pyx*xt))/prob; //if tails is observed, the chance of flipped coin being x is: (prob that last was x)(prob stays x)(prob tails w/ x) +
+    
+            probX = ((probX*pxx*xt)+(probY*pyx*xt)); //if tails is observed, the chance of flipped coin being x is: (prob that last was x)(prob stays x)(prob tails w/ x) +
             // prob that last was y)(prob changes to x)(prob tails w/ x) all divided by total probabilty
 
-            probY = ((probX*pxy*yt)+(probY*pyy*yt))/prob; // if tails is observed, the chance of flipped coin being y is: (prob that last was x)(prob changes to y)(prob tails w/ y) +
-            //(prob that last was y)(prob stays y)(prob tails w/ y) all divided by total probabilty
+            probY = ((probX*pxy*yt)+(probY*pyy*yt)); // if tails is observed, the chance of flipped coin being y is: (prob that last was x)(prob changes to y)(prob tails w/ y) +
+            //(prob that last was y)(prob stays y)(prob tails w/ y) all divided by total probability
 
-            sum = probX + probY; //debug line
+            prob = probX + probY; 
 
-            if (probX > probY){
-                cout << probX << "  " << probY << " sum " << sum << endl; //debug line
+            actX = probX/prob;
+            actY = probY/prob;
+
+            if (actX > actY){
                 result.push_back(0); // if there is a higher probability of coin x being the one that was flipped, we put a 0 to represent that in the result vector
             }
 
             else{
-                 cout << probX << "  " << probY << " sum " << sum << endl;//debug line
                 result.push_back(1); // if there is a higher probability of coin y being the one that was flipped, we put a 1 to represent that in the result vector
             }
         }
 
         else{
-            prob = (probX*pxx*xh + probX*pxy*yh + probY*pyy*yh + probY*pyx*xh);//if a heads is observed, the total probability is:  (prob that last was x)(prob stays x)(prob heads w/ x) +
- // (prob that last was x)(prob changes to y)(prob heads w/ y) + (prob that last was y)(prob stays y)(prob heads w/ y) + (prob that last was y)(prob changes to x)(prob heads w/ x)
+            probX = ((probX*pxx*xh)+(probY*pyx*xh));//if heads is observed, the chance of flipped coin being x is: (prob that last was x)(prob stays x)(prob heads w/ x) +
+            // prob that last was y)(prob changes to x)(prob heads w/ x) all divided by total probability
 
-            probX = ((probX*pxx*xh)+(probY*pyx*xh))/prob;//if heads is observed, the chance of flipped coin being x is: (prob that last was x)(prob stays x)(prob heads w/ x) +
-            // prob that last was y)(prob changes to x)(prob heads w/ x) all divided by total probabilty
+            probY = ((probX*pxy*yh)+(probY*pyy*yh));// if heads is observed, the chance of flipped coin being y is: (prob that last was x)(prob changes to y)(prob heads w/ y) +
+            //(prob that last was y)(prob stays y)(prob heads w/ y) all divided by total probability
+            prob = probX + probY; 
 
-            probY = ((probX*pxy*yh)+(probY*pyy*yh))/prob;// if heads is observed, the chance of flipped coin being y is: (prob that last was x)(prob changes to y)(prob heads w/ y) +
-            //(prob that last was y)(prob stays y)(prob heads w/ y) all divided by total probabilty
+            actX = probX/prob;
+            actY = probY/prob;
 
-            sum = probX + probY;  //debug line
-            if (probX > probY){
-                 cout << probX << "  " << probY << " sum " << sum << endl; //debug line
+            if (actX > actY){
                 result.push_back(0); // if there is a higher probability of coin x being the one that was flipped, we put a 0 to represent that in the result vector
             }
             else{
-                 cout << probX << "  " << probY << " sum " << sum << endl; //debug line
                 result.push_back(1); // if there is a higher probability of coin y being the one that was flipped, we put a 1 to represent that in the result vector
             }
         }   
     }
-     for( int i = 0; i < 20; i++){    //debug line
-        cout << observed[i] << endl;  //debug line
-    }
-    cout << "halfway" << endl;        //debug line
-    for( int i = 0; i < 20; i++){     //debug line
-        cout << result.at(i) << endl; //debug line
-    }
+    cout << "Output File:" << endl;
+    cin >> fname4;
+   ofstream out;
+   out.open(fname4);
+   for(int i = 0; i < result.size(); i++){
+    out << result[i] << " ";
+   }
     return 0;
 }
