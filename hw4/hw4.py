020b6c993e6b44f80476dfc28d49ac3e7bf306ef
orourkrl <orourkrl@clarkson.edu>
2023-12-10 12:28:02 -0500

    HW4 functioning, needs to be able to take graph from file + determine source and sink

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -1,80 +1,111 @@
-import graphviz as gz
-from graphviz import Digraph
-from collections import defaultdict
+import sys
+import queue
+import graphviz
 
-
-# ChatGPT code, trying to test it
 class Graph:
-    def __init__(self):
-        self.graph = defaultdict(dict)
+    def __init__(self, vertices):
+        self.vertices_set = vertices
+        self.graph = [[0] * self.vertices_set for _ in range(self.vertices_set)]
+        self.parent = [-1] * self.vertices_set
+        self.can_reach = set()
 
-    def add_edge(self, u, v, capacity):
-        self.graph[u][v] = capacity
+    def add_edge(self, start, end, capacity):
+        self.graph[start][end] = capacity
 
-    def min_cut(self, source, terminal):
-        residual_graph = self.graph.copy()
+    def bfs(self, start, goal):
+        visited = [False] * self.vertices_set
+        bfs_queue = queue.Queue()
+        bfs_queue.put(start)
+        visited[start] = True
 
-        def dfs(node, visited):
-            visited.add(node)
-            for neighbor, capacity in residual_graph[node].items():
-                if neighbor not in visited and capacity > 0:
-                    dfs(neighbor, visited)
+        while not bfs_queue.empty():
+            top = bfs_queue.get()
+            for node, capacity in enumerate(self.graph[top]):
+                if not visited[node] and capacity > 0:
+                    bfs_queue.put(node)
+                    visited[node] = True
+                    self.parent[node] = top
+                    if node == goal:
+                        return True
 
+        return False
+
+    # Calculating value of flow
+    def calc_flow(self, source, sink):
         max_flow = 0
-        while True:
-            visited = set()
-            dfs(source, visited)
-            
-            # Check if terminal is reachable from source
-            if terminal in visited:
-                # Find minimum capacity in the augmenting path
-                min_capacity = float('inf')
-                path = [(source, float('inf'))]
-                for u, v in zip(path, path[1:]):
-                    min_capacity = min(min_capacity, residual_graph[u[0]][v[0]])
-                
-                # Update residual graph
-                for u, v in zip(path, path[1:]):
-                    residual_graph[u[0]][v[0]] -= min_capacity
-                    residual_graph[v[0]][u[0]] += min_capacity
-                
-                max_flow += min_capacity
-            else:
-                break
 
-        # Now the residual graph contains the minimum cut
-        cut = set()
-        dfs(source, cut)
+        while self.bfs(source, sink):
+            path_flow = sys.maxsize
+            start = sink
+            while start != source:
+                path_flow = min(path_flow, self.graph[self.parent[start]][start])
+                start = self.parent[start]
 
-        return max_flow, cut
+            max_flow += path_flow
 
-    def visualize(self, filename='graph'):
-        dot = Digraph(comment='Flow Network')
-        for u, neighbors in self.graph.items():
-            dot.node(str(u))
-            for v, capacity in neighbors.items():
-                dot.edge(str(u), str(v), label=str(capacity))
+            curr_node = sink
+            while curr_node != source:
+                top = self.parent[curr_node]
+                self.graph[top][curr_node] -= path_flow
+                self.graph[curr_node][top] += path_flow
+                curr_node = top
+
+            self.can_reach = set()
+            self.dfs_reachable(source)
+
+        return max_flow
+
+    # Performing DFS to find nodes that can be reached from the source
+    def dfs_reachable(self, node):
+        self.can_reach.add(node)
+        for curr_node, capacity in enumerate(self.graph[node]):
+            if capacity > 0 and curr_node not in self.can_reach:
+                self.dfs_reachable(curr_node)
+
+    # Printing graph to PNG
+    def make_graph(self, filename='graph'):
+        dot = graphviz.Digraph(comment='Flow')
+        for i in range(self.vertices_set):
+            for j in range(self.vertices_set):
+                if self.graph[i][j] > 0:
+                    dot.edge(str(i), str(j), label=str(self.graph[i][j]))
+
         dot.render(filename, format='png', cleanup=True)
+        print(f"Graph saved as '{filename}.png'.")
 
-# Example usage:
-graph = Graph()
-graph.add_edge('source', 'A', 10)
-graph.add_edge('source', 'B', 5)
-graph.add_edge('A', 'B', 15)
-graph.add_edge('A', 'terminal', 10)
-graph.add_edge('B', 'terminal', 10)
+    # Taking in graph from text file
+    def read_graph(self, filename):
+        with open(filename, 'r') as file:
+            lines = file.readlines
 
-source = 'source'
-terminal = 'terminal'
+        for line in lines:
+            if '->' in lines:
+                halves = line.strip().split('->')
+                start = int(halves[0].strip())
+                end = int(halves[1].split('[')[0].strip())
+                capacity = int(halves[1].split('[')[1].split(']')[0].strip())
+                self.add_edge(start, end, capacity)
 
-graph.visualize('original_graph')
-
-max_flow, min_cut = graph.min_cut(source, terminal)
-
-print(f"Maximum Flow: {max_flow}")
-print(f"Minimum Cut: {min_cut}")
-
-graph.visualize('residual_graph')
-
+                # if self.source is None or start == self.sink:
+                #     self.source = start
+                # if self.sink is None or end == self.source:
+                #     self.sink = end
 
 
+
+if __name__ == "__main__":
+
+    num_vertices = 4
+    source = 0
+    sink = 3
+    graph1 = Graph(num_vertices)
+
+    graph1.read_graph("C://Users//euph1//OneDrive//Documents//GitHub//CS447//hw4//k4-minus-edge.txt")
+
+    flow = graph1.calc_flow(source, sink)
+    print(f"Maximum Flow: {flow}")
+    
+    witness_cut = graph1.can_reach
+    print(f"Witness Cut: {witness_cut}")
+
+    graph1.make_graph('graph')
