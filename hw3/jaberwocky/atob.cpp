48ecbdb0aa2e1f54e5c7e4ca81051223e7d331e4
sophiacarlone <sophiamcar@gmail.com>
2023-12-12 19:00:34 -0500

    jabberwocky start

---
+++ hw3/jaberwocky/atob.cpp
@@ -0,0 +1,16 @@
+//now onto jabber and wocky
+
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+	string input= argv[1];
+	string output= argv[2];
+	ifstream in;
+    ofstream out;
+    in.open(input);
+    out.open(output);
+
+}
---
+++ hw3/jaberwocky/jabber.cpp
@@ -0,0 +1,12 @@
+//now onto jabber and wocky
+
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+	string filename = argv[1];
+	fstream in;
+
+}
