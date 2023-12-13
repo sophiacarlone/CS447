151e33b7ca2f402bf2ef4e8fcd3442bbbcc5fef9
sophiacarlone <sophiamcar@gmail.com>
2023-12-13 18:45:33 -0500

    break

--- hw3/jaberwocky/jabber.cpp
+++ hw3/jaberwocky/jabber.cpp
@@ -2,11 +2,39 @@
 
 #include<iostream>
 #include<fstream>
+#include<map>
+#include<stdlib.h>
 
 using namespace std;
 
 int main(int argc, char *argv[]){
-	string filename = argv[1];
-	fstream in;
+	string input= argv[1];
+	string output= argv[2];
+	ifstream in;
+    ofstream out;
+    in.open(input);
+    out.open(output);
 
+	map<string, pair<int, int>> index_string_table;
+	index_string_table[""] = {0, 0}; //first line of table
+
+	while(!in.eof()){
+
+	}
+	in.close();
+	out.close();
 }
+
+	the input is a sequence of bits.
+	it maintains a stretchy (resizable) table T of patterns.
+		initially, T contains a single pattern (empty string) at index 0.
+	it maintains a running counter of bit-length W.
+		initially, W = 0.
+	while there are more bits in the input stream:
+		find the longest pattern x in T matching that is a prefix of the input.
+		say T[k] = x.
+		let the next input bit after x be b.
+		output index k in W bits followed by b.
+		add pattern x.b to the table T at the next available index.
+		increment W if need be (#items in T is a power of two)
+	end
