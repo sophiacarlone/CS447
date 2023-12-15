271d9afbb8e7b6a0f5afd29261f210b736ab1304
sophiacarlone <sophiamcar@gmail.com>
2023-12-14 19:13:47 -0500

    jabberwocky cleaned up

--- hw3/jaberwocky/jabber.cpp
+++ hw3/jaberwocky/jabber.cpp
@@ -32,29 +32,21 @@ int main(int argc, char *argv[]){
 	string curr_string = "";
 	int len;
 	while(in >> curr){
-		cout  << "top" << endl;
-		cout << curr << endl;
 		curr_string += curr;
 		auto it = index_string_table.find(curr_string);
 		if(it == index_string_table.end()){
-			cout << "hit b" << endl;
 			len = length(index); //length
 			index_string_table[curr_string] = {index, len};
-			cout << "entering table: " << index << " " << curr_string << " " << len << endl;
 			out << indexRep(index_string_table.find(prefix)->second.first, maxlen) << curr;
 			if(len > maxlen) maxlen = len;
-			cout << curr_string << endl;
 			index++;
 			curr_string = "";
 		}
 		else{
-			cout << "hit c" << endl;
 			prefix = curr_string;
 		}
 	}
 	if(prefix != ""){
-		cout << "hit" << endl;
-		cout << indexRep(index_string_table.find(prefix)->second.first, maxlen) << endl;
 		out << 	indexRep(index_string_table.find(prefix)->second.first, maxlen);
 	}
 	in.close();
@@ -69,21 +61,5 @@ int length(int index){
 string indexRep(int index, int len){
 	bitset<64> b(index);
 	string result = b.to_string();
-	cout  << "aaah" << endl;
-	cout << result << " " << len << endl;
 	return result.substr(result.size() - len);
 }
-
-	// the input is a sequence of bits.
-	// it maintains a stretchy (resizable) table T of patterns.
-	// 	initially, T contains a single pattern (empty string) at index 0.
-	// it maintains a running counter of bit-length W.
-	// 	initially, W = 0.
-	// while there are more bits in the input stream:
-	// 	find the longest pattern x in T matching that is a prefix of the input.
-	// 	say T[k] = x.
-	// 	let the next input bit after x be b.
-	// 	output index k in W bits followed by b.
-	// 	add pattern x.b to the table T at the next available index.
-	// 	increment W if need be (#items in T is a power of two)
-	// end
--- hw3/jaberwocky/wocky.cpp
+++ hw3/jaberwocky/wocky.cpp
@@ -34,25 +34,18 @@ int main(int argc, char *argv[]){
 	string curr_index = "";
 	int len;
     while(!in.eof()){
-        cout << "hit" << endl;
         for(int i = 0; i < maxlen; i++){
-            cout << "hit A" << endl;
             in >> curr;
             if(in.eof()){
-                cout << "hit end " << curr_index << endl;
                 curr = '\0';
                 continue;
             }
-            cout << "current for number " << curr << endl;
             curr_index += curr;
         }
         in >> curr;
-        cout << "current " << curr << endl;
-        cout << "index found " << string2int(curr_index) << endl;
         auto it = index_string_table.find(string2int(curr_index));
         if(curr == '\0') curr_string =  it->second.first;
         else curr_string =  it->second.first + curr;;
-        cout << "string " << curr_string << endl;
         out << curr_string;
         maxlen = length(index);
         index_string_table[index] = {curr_string, maxlen};
@@ -77,12 +70,9 @@ string indexRep(int index, int len){
 
 int string2int(string s){
     int number = 0;
-    //cout << "string: " << s << endl;
     reverse(s.begin(), s.end());
-    // char curr;
     for(int i = 0; i < s.size(); i++){
         if(s[i] == '1') number += pow(2, i);
-        // cout << "number " << number << endl;
     }
     return number;
 }
