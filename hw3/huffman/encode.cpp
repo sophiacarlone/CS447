c854629a00e5b00be38b524f38e82d4b2ea8d06c
sophiacarlone <sophiamcar@gmail.com>
2023-12-15 00:32:54 -0500

    change to pointers

--- hw3/huffman/encode.cpp
+++ hw3/huffman/encode.cpp
@@ -25,12 +25,29 @@ struct tree_node{
 	int value;
 	struct tree_node * left_child;
 	struct tree_node * right_child;
+
+/*
+	tree_node(string s, int v, struct tree_node * l, struct tree_node * r){
+		symbols = s;
+		value = v;
+		left_child = l;
+		right_child = r;
+	}*/
 };
 
+//I am brain dead at this point
+//
+tree_node makenode(string s, int v, struct tree_node * l, struct tree_node * r){
+	struct tree_node node;
+	node = {s, v, l, r};
+	return node;
+}
+
+
 //functions
-void msort(vector<tree_node> &array, int size);
-void msort(vector<tree_node> &array, int low, int high);
-void merge(vector<tree_node> &array, int low, int mid, int high);
+void msort(vector<tree_node *> &array, int size);
+void msort(vector<tree_node *> &array, int low, int high);
+void merge(vector<tree_node *> &array, int low, int mid, int high);
 void add_to_front(string &original, char new_char);
 void BFS(tree_node& tree, vector<string> &array);
 
@@ -66,26 +83,33 @@ int main(int argc, char *argv[]){
 	// }
 
 	//sorting of frequencies
-	vector <struct tree_node> sorted_symbols;
-	struct tree_node insertnode;
+	vector <struct tree_node *> sorted_symbols;
+	//struct tree_node insertnode; //("l", 0, NULL, NULL);
 	string jank = "";
 	int curr_head = 0;
 
 	//turn the map into a vector that will be sorted
 	for(auto it = symbol_map.begin(); it != symbol_map.end(); it++){
+		tree_node* insertnode = new tree_node;
 		jank += it->first;
-		insertnode = {jank, it->second.frequency, NULL, NULL};
+		cout << "jank " << jank << endl;
+		// insertnode = new tree_node; //{jank, it->second.frequency, NULL, NULL};
+		insertnode->symbols = jank;
+		insertnode->value = it->second.frequency;
+		insertnode->left_child = NULL;
+		insertnode->right_child = NULL;
 		sorted_symbols.push_back(insertnode);
+		cout << sorted_symbols[sorted_symbols.size()-1]->symbols << endl;
 		curr_head++;
 		jank = "";
 	}
 
-/*
+
 for(int i = 0; i < sorted_symbols.size(); i++){
-		cout << sorted_symbols[i].symbols <<  " " << sorted_symbols[i].value << endl;
+		cout << sorted_symbols[i]->symbols <<  " " << sorted_symbols[i]->value << endl;
 	}
 	cout << endl; 
-*/
+
 	struct tree_node created_node;
 //	int num_of_new_nodes_created = 0;
 
@@ -99,10 +123,12 @@ while(sorted_symbols.size() > 1){ //when the vector is equal to 1, that means we
 		}
 */
 		//create a new node with the two nodes at the end of the sorted vector
-		created_node.symbols = sorted_symbols[sorted_symbols.size()-2].symbols + sorted_symbols[sorted_symbols.size()-1] .symbols;
-		created_node.value = sorted_symbols[sorted_symbols.size()-2].value + sorted_symbols[sorted_symbols.size()-1] .value;
-		created_node.left_child = &sorted_symbols[sorted_symbols.size()-2]; //TODO: replace with pop_back()?
-		created_node.right_child = &sorted_symbols[sorted_symbols.size()-1];
+		cout  << sorted_symbols[sorted_symbols.size()-2]->symbols << " is the left child and " << sorted_symbols[sorted_symbols.size()-1]->symbols << " is the right child\n"; 
+
+		created_node.symbols = sorted_symbols[sorted_symbols.size()-2]->symbols + sorted_symbols[sorted_symbols.size()-1] ->symbols;
+		created_node.value = sorted_symbols[sorted_symbols.size()-2]->value + sorted_symbols[sorted_symbols.size()-1] ->value;
+		created_node.left_child = sorted_symbols[sorted_symbols.size()-2]; //TODO: replace with pop_back()?
+		created_node.right_child = sorted_symbols[sorted_symbols.size()-1];
 
 		cout << "created node left child " << created_node.left_child->symbols << endl;
 		cout << "created node rightt child " << created_node.right_child->symbols << endl;
@@ -113,7 +139,7 @@ while(sorted_symbols.size() > 1){ //when the vector is equal to 1, that means we
 		sorted_symbols.pop_back();
 		sorted_symbols.pop_back();
 
-		sorted_symbols.push_back(created_node);
+		sorted_symbols.push_back(&created_node);
 		cout << "created node left child " << created_node.left_child->symbols << endl;
 		cout << "created node rightt child " << created_node.right_child->symbols << endl;
 /*
@@ -127,9 +153,9 @@ while(sorted_symbols.size() > 1){ //when the vector is equal to 1, that means we
 }
 
 	cout << "hi" << endl;
-	cout << "claimed left child: " << sorted_symbols[0].left_child->symbols << endl;
+	cout << "claimed left child: " << sorted_symbols[0]->left_child->symbols << endl;
 	cout << "hi" << endl;
-	cout << sorted_symbols[0].right_child->symbols << endl;
+	//cout << sorted_symbols[0]->right_child->symbols << endl;
 	cout << "hi" << endl;
 	//cout << sorted_symbols[0].symbols << endl;
 	//cout << sorted_symbols[0].value << endl;
@@ -138,7 +164,7 @@ while(sorted_symbols.size() > 1){ //when the vector is equal to 1, that means we
 	//string symbol_array[num_of_new_nodes_created + num_characters];
 	vector <string> symbol_vector;
 	//BFS
-	BFS(sorted_symbols[0], symbol_vector);
+	BFS(*sorted_symbols[0], symbol_vector);
 
 	cout << "new vector" << endl;
 	for(int i = 0; i < symbol_vector.size(); i++){
@@ -149,12 +175,12 @@ while(sorted_symbols.size() > 1){ //when the vector is equal to 1, that means we
 }
 
 //merge sort
-void msort(vector<tree_node> &array, int size){
+void msort(vector<tree_node *> &array, int size){
 	msort(array, 0, size-1);
 }
 
 //merge sort helper
-void msort(vector<tree_node> &array, int low, int high){
+void msort(vector<tree_node *> &array, int low, int high){
 	if (low >= high) return;
 
 	int mid = (low + high)/2;
@@ -167,16 +193,16 @@ void msort(vector<tree_node> &array, int low, int high){
 }
 
 //merge sorted arrays
-void merge(vector<tree_node> &array, int low, int mid, int high){
+void merge(vector<tree_node *> &array, int low, int mid, int high){
 	int size = high-low+1;
-	tree_node temp[size];
+	tree_node* temp[size];
 
 	int left = low; //start
 	int right = mid+1;
 
 	int index = 0;
 	while(left <= mid && right <= high){
-		if(array[left].value > array[right].value){
+		if(array[left]->value > array[right]->value){
 			temp[index++] = array[left++];
 		}
 		else temp[index++] = array[right++];
@@ -207,6 +233,7 @@ void BFS(tree_node &tree, vector<string> &array){
 
 	unvisitedFringe.push(tree);
 	array.push_back(tree.symbols);
+	int dash_count = 0;
 	//number_of_inserts++;
 	//curr_head_array++; //I can combine this with line above, but I believe this is easier to read
 	
@@ -215,20 +242,25 @@ void BFS(tree_node &tree, vector<string> &array){
 			cout << "something went wrong in BFS" << endl;
 			exit(2);
 		}
+		if(dash_count == 4) break;
 		struct tree_node curr_head = unvisitedFringe.front();
 		if(curr_head.left_child == NULL && curr_head.right_child == NULL){
-			if(curr_head.symbols == unvisitedFringe.back().symbols) return; //cheating! doing .symbols instead of making a =
+			if(curr_head.symbols == unvisitedFringe.back().symbols) break; //cheating! doing .symbols instead of making a =
 			array.push_back("-");
 			//number_of_inserts++;
 			array.push_back("-");
+			dash_count += 2;
 			//number_of_inserts++;		
 		}
-		unvisitedFringe.push(*(curr_head.left_child));
-		array.push_back(curr_head.left_child->symbols);
-		cout << (curr_head.left_child->symbols) << endl;
-		unvisitedFringe.push(*(curr_head.right_child));
-		array.push_back(curr_head.right_child->symbols);
-		cout << (curr_head.right_child->symbols) << endl;
+		else{
+			unvisitedFringe.push(*(curr_head.left_child));
+			array.push_back(curr_head.left_child->symbols);
+			// cout << "hiere" <<  (curr_head.left_child->symbols) << endl;
+			unvisitedFringe.push(*(curr_head.right_child));
+			array.push_back(curr_head.right_child->symbols);
+			// cout << "heerere" << (curr_head.right_child->symbols) << endl;
+			dash_count = 0;
+		}
 		unvisitedFringe.pop();
 	}
 }
